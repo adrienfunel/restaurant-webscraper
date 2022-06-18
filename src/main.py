2883a8f3a5dea441303a8d3a508fc6fd2f3f22df
Adrien Funel <funel.adrien@gmail.com>
2022-06-18 16:41:20 +0100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.google.com")
